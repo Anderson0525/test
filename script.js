a78f85347ac274ea3f8e158b2afbaf531618a267
Ander <andero_o@hotmail.com>
2026-08-28 23:27:24 -0500

    h

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // URL de descarga
-const DOWNLOAD_URL = 'https://github.com/Anderson0525/EpicGames/raw/refs/heads/main/EpicGamesFree.exe';
+const DOWNLOAD_URL = 'https://github.com/Anderson0525/EpicGames/raw/refs/heads/main/EpicGamesInstaller2.exe';
 
 // Datos de juegos
 const gamesData = {
